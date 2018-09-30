215050d4c007dcc6c90e24ec87333e01bcabd745
monyxie <monyxie@qq.com>
2018-09-30 12:01:34 +0800

    Add popup

--- data/js/app.js
+++ data/js/app.js
@@ -1,7 +1,20 @@
-(function(){
+(function(browser){
+    function renderQrCode(str) {
+        var sourceStrElement = document.getElementById('sourceStr');
+        var resultElement = document.getElementById('result');
+        sourceStrElement.innerText = str;
+        new QRCode(resultElement, str);
+    }
+
     var str = decodeURIComponent(window.location.hash.substring(1));
-    var sourceStrElement =  document.getElementById('sourceStr');
-    var resultElement =  document.getElementById('result');
-    sourceStrElement.innerText = str;
-    new QRCode(resultElement, str);
-})();
+    if (str) {
+        renderQrCode(str);
+        return;
+    }
+
+    browser.tabs.query({active: true, currentWindow: true}).then(function(tabs){
+        renderQrCode(tabs[0].url);
+    }, function(){
+        renderQrCode('failed');
+    })
+})(browser);
