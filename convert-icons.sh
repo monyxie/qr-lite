86003744277fbbf4653f4997df942f2bf47589b0
Max Xie <monyxie@gmail.com>
2024-05-22 23:13:34 +0800

    Use one universal extension icon in all places

--- convert-icons.sh
+++ convert-icons.sh
@@ -5,17 +5,7 @@
 
 set -xe
 
-for SIZE in 16 32 64 128 256 512 1024
+for SIZE in 16 32 48 128
 do
   inkscape -w "$SIZE" -h "$SIZE" "src/icons/qrlite.svg" -o "src/icons/qrlite-$SIZE.png"
-done
-
-for SIZE in 16 32 64 128 256 512 1024
-do
-  inkscape -w "$SIZE" -h "$SIZE" "src/icons/qrlite-dark.svg" -o "src/icons/qrlite-dark-$SIZE.png"
-done
-
-for SIZE in 16 32 64 128 256 512 1024
-do
-  inkscape -w "$SIZE" -h "$SIZE" "src/icons/qrlite-light.svg" -o "src/icons/qrlite-light-$SIZE.png"
 done
